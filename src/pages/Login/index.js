2060711d62c49399caf04c6e641663ef0f249754
Bruno Germano <brunoalvesgermano@gmail.com>
2022-11-20 00:39:15 -0300

    Remove Mui with styled components

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import TextField from '@mui/material/TextField';
+//import TextField from '@mui/material/TextField';
 
 
 function Login() {
   return (
     <>
       <h1>Bem-vindo</h1>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      {//<TextField id="outlined-basic" label="Outlined" variant="outlined" />
+}
     </>
   )
 }
