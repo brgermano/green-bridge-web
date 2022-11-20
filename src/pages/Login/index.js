fe8c3cd516189251cdc84eb27c5262e0369e95bb
Bruno Germano <brunoalvesgermano@gmail.com>
2022-11-20 00:37:03 -0300

    Add Login and Home folders

---
+++ src/pages/Login/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TextField from '@mui/material/TextField';
+
+
+function Login() {
+  return (
+    <>
+      <h1>Bem-vindo</h1>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </>
+  )
+}
+
+export default Login;
